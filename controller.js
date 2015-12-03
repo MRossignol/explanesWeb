f85f6d02b834082df1549fdf02c21922702f1402
Mathias Rossignol <mathias.rossignol@gmail.com>
2015-12-03 09:01:52 +0700

    Initial commit

---
+++ controller.js
@@ -0,0 +1,17 @@
+app.controller("PageController", function($scope) {
+    $scope.activeSection = 0;
+    $scope.textHeight = 0;
+
+    $scope.panels = [
+	{"name":"Overview", "page":"pages/overview.html"},
+	{"name":"Get started", "page":"pages/getting_started.html"},
+	{"name":"Documentation", "page":"pages/documentation.html"},
+	{"name":"Demo", "page":"pages/demo.html"},
+	{"name":"Get involved", "page":"pages/involved.html"},
+    ];
+
+    $scope.setSection = function (i) {
+	$scope.activeSection = i;
+	$scope.textHeight = document.getElementById('panel-'+i).offsetHeight;
+    }
+});
