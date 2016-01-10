b6ae4f19283a36162b4f1404f1102ddaa0111f12
Mathias Rossignol <mathias.rossignol@gmail.com>
2016-01-10 20:49:51 +0700

    Improved profile carousel

--- controller.js
+++ controller.js
@@ -38,32 +38,36 @@ app.controller("PageController", function($scope, $timeout, $interval) {
     }
 
     $scope.slides = [
-	{position: 0, text:"Master", photo:"images/slide-1.jpg", color:"#eea", textOpacity: 0, bullets:[
+	{position: 0, text:"Master", photo:"images/slide-1.jpg", thumbnail:"images/thumb-profile-1.png", color:"#eea", textOpacity: 0, bullets:[
 	    {text:"First steps in the scientific community"},
 	    {text:"Compare your work with existing references"}]
 	},
-	{position: 1, text:"Ph.D.", photo:"images/slide-2.jpg", color:"#aae", textOpacity: 0, bullets:[
+	{position: 1, text:"Ph.D.", photo:"images/slide-2.jpg", thumbnail:"images/thumb-profile-2.png", color:"#aae", textOpacity: 0, bullets:[
 	    {text:"Keep track of large amount of code and experiments"},
 	    {text:"Make your experimental results publishable"},
 	    {text:"Stay focused and efficient on the long run"}]
 	},
-	{position: 2, text:"Postdoctoral fellow", photo:"images/slide-3.jpg", color:"#aea", textOpacity: 0, bullets:[
+	{position: 2, text:"Postdoctoral fellow", photo:"images/slide-3.jpg", thumbnail:"images/thumb-profile-3.png", color:"#aea", textOpacity: 0, bullets:[
 	    {text:"Stay organized, with many different projects to juggle at once"},
 	    {text:"Publish!"}]
 	},
-	{position: 3, text:"Tenured researcher", photo:"images/slide-4.jpg", color:"#eaa", textOpacity: 0, bullets:[
+	{position: 3, text:"Tenured researcher", photo:"images/slide-4.jpg", thumbnail:"images/thumb-profile-4.png", color:"#eaa", textOpacity: 0, bullets:[
 	    {text:"Easily switch between numerous projects"},
 	    {text:"Keep your computer working for you while you're in meetings"},
 	    {text:"Standardize the way your students organize their code and data"}]
 	}
     ];
 
+    var showBulletTimeouts = [];
+
     var hideBullets = function(slide) {
 	var nbBullets = slide.bullets.length;
 	var startY = -100;
 	var endY = 500;
-	for (var i=0; i<nbBullets; i++)
-	    slide.bullets[i].style = {left:'810px', top:Math.round(startY+i*(endY-startY)/(nbBullets+1))+'px', opacity: 0, fontSize:'60px', width:'1400px'};
+	$timeout(function() {
+	    for (var i=0; i<nbBullets; i++)
+		slide.bullets[i].style = {left:'810px', top:Math.round(startY+i*(endY-startY)/(nbBullets+1))+'px', opacity: 0, fontSize:'60px', width:'1400px'};
+	}, 300);
     }
 
     var showBullet = function(slide, i) {
@@ -75,70 +79,55 @@ app.controller("PageController", function($scope, $timeout, $interval) {
 
     var showBullets = function (slide) {
 	for (var i=0, l=slide.bullets.length; i<l; i++)
-	    $timeout(showBullet.curry(slide, i), 800+2500*i/l)
-    }
-
-    var nbSlices = 2;
-    var showSlide = function (slide) {
-	slide.slices = [];
-	var width = 800;
-	var sliceSizes = [];
-	var total = 0;
-	for (var i=0; i<nbSlices; i++) {
-	    var r = .1+Math.random();
-	    total += r;
-	    sliceSizes.push(r);
-	}
-	var accW = 0;
-	var acc = 0;
-	for (var i=0; i<nbSlices; i++) {
-	    var w = Math.round(800*((sliceSizes[i]+acc)/total))-accW;
-	    slide.slices.push({
-		x: Math.floor(800*Math.random()),
-		w: 0,
-		targetX: accW,
-		targetW: w
-	    });
-	    accW += w;
-	    acc += sliceSizes[i];
-	}
-	// $timeout(function () {
-	//     for (var i=0; i<nbSlices; i++) {
-	// 	slide.slices[i].x = slide.slices[i].targetX+(slide.slices[i].targetX-slide.slices[i].x)/2;
-	// 	slide.slices[i].w = slide.slices[i].targetW/3;
-	//     }}, 0);
-	$timeout(function () {
-	    for (var i=0; i<nbSlices; i++) {
-		slide.slices[i].x = slide.slices[i].targetX;
-		slide.slices[i].w = slide.slices[i].targetW;
-		slide.textOpacity = 1;
-	    }}, 0);
-	showBullets(slide);
+	    showBulletTimeouts.push($timeout(showBullet.curry(slide, i), 800+2500*i/l));
     }
     
-    var cycleSlides = function() {
+    var targetSlide = null;
+
+    $scope.cycleTo = function (slide) {
+	targetSlide = slide;
+	$scope.cycleSlides();
+    }
+
+    $scope.cycleSlides = function(dir) {
+	if (targetSlide) {
+	    $interval.cancel($scope.profileInterval);
+	    if (targetSlide.position == 0) {
+		targetSlide = null;
+		return;
+	    } else {
+		dir = targetSlide.position / Math.abs(targetSlide.position);
+	    }
+	} else if (dir) {
+	    if (dir<0) dir = -1;
+	    else dir = 1;
+	    $interval.cancel($scope.profileInterval);
+	} else {
+	    dir = 1;
+	}
+	showBulletTimeouts.forEach(function(t){$timeout.cancel(t);});
+	showBulletTimeouts = [];
 	for (var s=0, nbSlides = $scope.slides.length; s<nbSlides; s++) {
 	    var slide = $scope.slides[s];
-	    slide.position--;
-	    if (slide.position == -2) {
-		slide.position += nbSlides;
-		slide.display = 'none';
+	    slide.position -= dir;
+	    slide.opacity = (!slide.position || dir*slide.position==-1) ? 1 : 0;
+	    if (slide.position == 0)
+		showBullets(slide);
+	    else
 		hideBullets(slide);
-	    } else if (slide.position == -1) {
-		slide.textOpacity = 0;
-	    } else if (slide.position == 0) {
-		slide.display='';
-		showSlide(slide);
-	    }
+	    if (dir*slide.position <= -2)
+		slide.position += dir*nbSlides;
 	}
+	if (targetSlide) $scope.cycleSlides();
     }
 
     for (var s=0, nbSlides = $scope.slides.length; s<nbSlides; s++)
 	hideBullets($scope.slides[s]);
     
-    $timeout(showSlide.curry($scope.slides[0]), 0);
+
+    $timeout(showBullets.curry($scope.slides[0]), 1000);
     
-    $interval(cycleSlides, 5000);
+    $scope.profileInterval = $interval($scope.cycleSlides.curry(0), 5000);
     
     $scope.$on('$includeContentLoaded', function (e) {console.log(e); $scope.setSection(0);})
     $scope.setSection(0);
